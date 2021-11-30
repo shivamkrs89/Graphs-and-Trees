8017c076d028ce7d0d815967c8c5f591da9052f7
shivamkrs89 <70107607+shivamkrs89@users.noreply.github.com>
2021-12-01 00:21:26 +0530

    Create bst_sumup_smallervalues__aka__enlarge_bst.cpp

---
+++ bst_sumup_smallervalues__aka__enlarge_bst.cpp
@@ -0,0 +1,66 @@
+Given a binary search tree root, replace every node's value v by its value plus the sum of all other values in the tree that are greater than v.
+
+Constraints
+
+n ≤ 100,000 where n is the number of nodes in root
+Example 1
+Input
+Visualize
+root =
+4
+
+2
+
+5
+
+1
+
+3
+
+Output
+Visualize
+9
+
+14
+
+5
+
+15
+
+12
+
+//code
+/**
+ * class Tree {
+ *     public:
+ *         int val;
+ *         Tree *left;
+ *         Tree *right;
+ * };
+ */
+int solveutil(Tree* root,int &psum)
+{
+    if(root==NULL)
+       return psum;
+
+    int addvr=solveutil(root->right,psum);
+    
+    //int real_val=root->val;
+    root->val+=(addvr);
+    psum=root->val;//keeps track of all smaller values
+    int addvl=solveutil(root->left,psum);
+    
+    
+
+    return psum;
+
+}
+
+
+Tree* solve(Tree* root) {
+    int psum=0;
+    solveutil(root,psum);
+
+    return root;
+}
+//[0, null, [2, [1, null, null], null]] //zig zag case
